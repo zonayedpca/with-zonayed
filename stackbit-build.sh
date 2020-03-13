c95cfedaf551ef55522cba40f1dbc8bdad18de41
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-03-13 17:25:21 +0600

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -11,6 +11,7 @@ else
     npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5e53a0ab89830a001a0d4967 
 fi
 curl -s -X POST https://api.stackbit.com/project/5e53a0ab89830a001a0d4967/webhook/build/ssgbuild > /dev/null
+node createapi
 gatsby build
 
 curl -s -X POST https://api.stackbit.com/project/5e53a0ab89830a001a0d4967/webhook/build/publish > /dev/null
