283c714e8ca9e8b0aaf5f4ee05723607daee0f59
Zonayed Ahmed <zonayedpca@yahoo.com>
2020-04-23 19:50:54 +0600

    Tags post combined

--- createapi.js
+++ createapi.js
@@ -3,6 +3,8 @@ const path = require("path");
 const parser = require("parser-front-matter");
 const showdown = require("showdown");
 
+const english = /^[A-Za-z0-9\-]*$/;
+
 const converter = new showdown.Converter();
 
 const postsDir = path.resolve("./src/pages/posts");
@@ -12,12 +14,12 @@ const postFiles = fs.readdirSync(postsDir);
 
 const postsByTag = {};
 
-if(fs.existsSync(apiDir)) {
+if (fs.existsSync(apiDir)) {
   fs.rmdirSync(apiDir, { recursive: true, force: true });
 }
 fs.mkdirSync(apiDir);
 
-postFiles.forEach(file => {
+postFiles.forEach((file) => {
   const str = fs.readFileSync(`${postsDir}/${file}`, "utf8");
   const res = parser.parseSync(str);
   const html = converter.makeHtml(res.content);
@@ -28,7 +30,7 @@ postFiles.forEach(file => {
     series,
     template,
     title,
-    thumb_img_path
+    thumb_img_path,
   } = res.data;
   if (tags) {
     const data = {
@@ -39,13 +41,15 @@ postFiles.forEach(file => {
       template,
       title,
       thumb_img_path,
-      content: html
+      content: html,
     };
-    tags.map(tag => {
-      if (postsByTag[tag]) {
-        postsByTag[tag].push(data);
-      } else {
-        postsByTag[tag] = [data];
+    tags.map((tag) => {
+      if (english.test(tag)) {
+        if (postsByTag[tag]) {
+          postsByTag[tag].push(data);
+        } else {
+          postsByTag[tag] = [data];
+        }
       }
     });
   }
@@ -55,10 +59,11 @@ const tagKeys = Object.keys(postsByTag);
 
 const mainIndexJson = {};
 
-tagKeys.map(tag => {
+tagKeys.map((tag) => {
   const tagDir = `${apiDir}/${tag}`;
   fs.mkdirSync(tagDir);
   const singleTagPosts = postsByTag[tag];
+  const tagPosts = [];
   singleTagPosts.map(
     (
       {
@@ -69,7 +74,7 @@ tagKeys.map(tag => {
         template,
         title,
         thumb_img_path,
-        content
+        content,
       },
       index
     ) => {
@@ -81,14 +86,28 @@ tagKeys.map(tag => {
         template,
         title,
         thumb_img_path,
-        contentDir: `${index}`
+        contentDir: `${index}`,
       };
       if (mainIndexJson[tag]) {
         mainIndexJson[tag].push(data);
       } else {
         mainIndexJson[tag] = [data];
       }
+      tagPosts.push({
+        language,
+        tags,
+        date,
+        series,
+        template,
+        title,
+        thumb_img_path,
+        content,
+      });
       const dataWithContent = { ...data, content };
+      fs.writeFileSync(
+        `${apiDir}/${tag}/index.json`,
+        JSON.stringify(dataWithContent)
+      );
       fs.writeFileSync(
         `${apiDir}/${tag}/${index}.json`,
         JSON.stringify(dataWithContent)
